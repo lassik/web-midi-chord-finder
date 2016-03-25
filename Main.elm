1f06c283bb222027434d0437dc79b310758d726e
Lassi Kortela <lassi@lassi.io>
2016-03-25 14:43:32 -0400

    Simplify Main

--- Main.elm
+++ Main.elm
@@ -4,12 +4,9 @@ import Signal exposing (..)
 import Task exposing (Task)
 
 {- basic plumbing just to get the WebMidi init task run -}
-myTaskMailbox : Mailbox (Task () ())
 myTaskMailbox = mailbox WebMidi.init
-
 port myTaskPort : Signal (Task () ())
-port myTaskPort = 
-  myTaskMailbox.signal
+port myTaskPort = myTaskMailbox.signal
 
 main : Signal Element
 main =
