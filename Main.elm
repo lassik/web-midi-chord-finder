3632cd329922449e0cf6fe081d6226faa9cdb1f0
Lassi Kortela <lassi@lassi.io>
2016-03-25 14:43:50 -0400

    Pitch set of currently depressed keys shown on screen! This is fun

--- Main.elm
+++ Main.elm
@@ -2,12 +2,13 @@ import WebMidi exposing (..)
 import Graphics.Element exposing (Element, show)
 import Signal exposing (..)
 import Task exposing (Task)
+import Set
 
 {- basic plumbing just to get the WebMidi init task run -}
 myTaskMailbox = mailbox WebMidi.init
 port myTaskPort : Signal (Task () ())
 port myTaskPort = myTaskMailbox.signal
 
-main : Signal Element
-main =
-  Signal.map show midiNoteS
+update m set = (if m.noteOn then Set.insert else Set.remove) m.pitch set
+pitchSet = foldp update Set.empty midiNoteS
+main = pitchSet |> Signal.map show
