0a45d309aaa3cd94d49baa7582b41ddc844eb571
Lassi Kortela <lassi@lassi.io>
2017-06-21 15:33:35 +0300

    Optimize elm.js using Closure Compiler

--- build.sh
+++ build.sh
@@ -2,3 +2,8 @@
 set -eux
 cd "$(dirname "$0")"
 elm-make --yes --warn --output=elm.js src/Main.elm
+closure-compiler \
+    --language_in=ECMASCRIPT5 \
+    --js=elm.js \
+    --js_output_file=elm.js.new
+mv elm.js.new elm.js
